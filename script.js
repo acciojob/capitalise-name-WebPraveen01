4bb26b0d6aa713172eda1157a50461ea27d01a54
WebPraveen01 <47769628+WebPraveen01@users.noreply.github.com>
2024-10-25 22:50:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// Select the input field
+var inputField = document.getElementById("fname");
+
+// Add event listener for the 'blur' event
+inputField.addEventListener('blur', function() {
+inputField.value = inputField.value.toUpperCase();
+
+});
